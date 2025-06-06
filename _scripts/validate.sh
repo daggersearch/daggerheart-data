67d0274525cb55a362bf6649d9c587d2ec7ff48e
Jordan Potter <me@jordanpotter.com>
2025-06-06 08:50:24 -0700

    Correct schema refs to mirror folder structure

--- _scripts/validate.sh
+++ _scripts/validate.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$0")"
 
 for type in ancestries armor communities consumables domain-cards items subclasses weapons; do
     for release in core; do
-        npx ajv --spec=draft2020 -r "../_schemas/common/**/*.json" -s "../_schemas/${type}.schema.json" -d "../${release}/${type}.json"
+        npx ajv validate --spec=draft2020 -r "../_schemas/common/**/*.json" -s "../_schemas/${type}.schema.json" -d "../${release}/${type}.json"
     done
 done
