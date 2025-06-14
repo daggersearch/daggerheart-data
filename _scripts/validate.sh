3ae3576d68a85843ee3e8abcb3a109d1247231d3
Jordan Potter <jordanpotter@users.noreply.github.com>
2025-06-13 17:06:03 -0700

    Begin to add rules (#6)
    Co-authored-by: Jordan Potter <me@jordanpotter.com>

--- _scripts/validate.sh
+++ _scripts/validate.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-for type in ancestries armors communities consumables domain-cards items subclasses weapons; do
+for type in rules ancestries armors communities consumables domain-cards items subclasses weapons; do
     for release in core; do
         npx ajv validate --spec=draft2020 -r "../_schemas/common/**/*.schema.json" -s "../_schemas/${type}.schema.json" -d "../${release}/${type}.json"
     done
