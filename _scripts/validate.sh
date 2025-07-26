e29debc02ef01ed303d3d9462a774fe48d0df49d
Jordan Potter <me@jordanpotter.com>
2025-07-25 19:30:12 -0700

    Add classes from core and the void

--- _scripts/validate.sh
+++ _scripts/validate.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-for type in rules ancestries armors communities consumables domain-cards items subclasses weapons; do
+for type in ancestries armors classes communities consumables domain-cards items rules subclasses weapons; do
     for release in core the_void; do
         npx ajv validate --spec=draft2020 -r "../_schemas/common/**/*.schema.json" -s "../_schemas/${type}.schema.json" -d "../${release}/${type}.json"
     done
